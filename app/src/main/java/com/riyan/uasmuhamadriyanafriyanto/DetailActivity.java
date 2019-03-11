fd409c256568cf87e70e458d3e64e2e7c5537b1d
Riyan28 <RiyanRFZ@gmail.com>
2019-03-11 21:25:17 +0700

    ok

---
+++ app/src/main/java/com/riyan/uasmuhamadriyanafriyanto/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.riyan.uasmuhamadriyanafriyanto;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class DetailActivity extends AppCompatActivity {
+    EditText d_ed_nama, d_ed_kelas;
+    Button bt_ubah, bt_hapus;
+
+    Bundle b;
+
+
+
+}
