2055f8028cc94087a4b45b52b12af87c68ef82f6
Riyan28 <RiyanRFZ@gmail.com>
2019-03-11 20:56:54 +0700

    Initial commit

---
+++ app/src/main/java/com/riyan/uasmuhamadriyanafriyanto/MainActivity.java
@@ -0,0 +1,56 @@
+package com.riyan.uasmuhamadriyanafriyanto;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        final Button Simpan = (Button) findViewById(R.id.btSimpan);
+        final Button Kosong = (Button) findViewById(R.id.btKosong);
+        final EditText Nama = (EditText) findViewById(R.id.et_nama);
+        final EditText Email = (EditText) findViewById(R.id.et_email);
+
+
+        Simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View parentView = findViewById(R.id.main_layout_id);
+                String pesan = "DATA TERSIMPAN";
+
+                final Snackbar snackbar = Snackbar
+                     .make(parentView, pesan, Snackbar.LENGTH_LONG );
+                snackbar.show();
+            }
+        });
+
+        Kosong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Nama.setText("");
+                Email.setText("");
+            }
+        });
+    }
+
+
+        public boolean onCreateOptionMenu(Menu menu){
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu_action_bar, menu);
+            return super.onCreateOptionsMenu(menu);
+        }
+}
